0124fead49db8bb15f11ee28399ac9279c2e0475
BitOfGold <info@bitofgold.com>
2025-06-16 14:53:24 +0200

    version 0.1

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,42 @@
 // vite.config.js
-import { resolve } from "path"
-import { defineConfig } from "vite"
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { externalizeDeps } from 'vite-plugin-externalize-deps'
+
+const config = {
+  server: {
+    entry: resolve(__dirname, './src/server.ts'),
+    name: 'TopazCube Server',
+    fileName: 'topazcube-server',
+  },
+  client: {
+    entry: resolve(__dirname, './src/topazcube.ts'),
+    name: 'TopazCube Client',
+    fileName: 'topazcube-client',
+  },
+}
+
+const currentConfig = config[process.env.LIB_NAME]
+
+if (currentConfig === undefined) {
+  throw new Error('LIB_NAME is not defined or is not valid')
+}
 
 export default defineConfig({
-  root: resolve(__dirname, "src"),
+  root: resolve(__dirname, 'src'),
+  plugins: [externalizeDeps()],
   build: {
     lib: {
-      entry: resolve(__dirname, "src/topazcube.js"),
-      name: "TopazCube",
-      // the proper extensions will be added
-      fileName: "topazcube",
-      formats: ["es"],
+      ...currentConfig,
+      formats: ['es'],
+      target: 'esnext',
     },
-    outDir: resolve(__dirname, "dist"),
-    emptyOutDir: true, 
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: false,
+    esbuild: {
+      comments: 'none',
+      legalComments: 'none',
+    },
     rollupOptions: {
       output: {
         manualChunks: undefined,
